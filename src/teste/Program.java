ab454f88d2f7b31d18e681091781bb356418ca8d
Orlando Ferreira <orlandofnfilho@gmail.com>
2022-05-08 22:59:26 -0300

    update

--- src/teste/Program.java
+++ src/teste/Program.java
@@ -1,27 +1,24 @@
 package teste;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;import java.util.stream.Collector;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
+
 		List<String> names = new ArrayList<String>();
-		
+
 		names.add("Jose");
 		names.add("Orlando");
 		names.add("Ferreira");
-		
-		//names.sort((n1, n2) -> n1.compareTo(n2));
-		
+
+		// names.sort((n1, n2) -> n1.compareTo(n2));
+
 		List<String> names2 = names.stream().map(x -> x.toUpperCase()).collect(Collectors.toList());
-		
+
 		names2.forEach(System.out::println);
-		
 
 	}
 
