7d11a86f4518fbf74a9dd2fece1eea2aa7d75312
Orlando Ferreira <orlandofnfilho@gmail.com>
2022-05-11 18:01:03 -0300

    update

--- src/teste/Program.java
+++ src/teste/Program.java
@@ -1,24 +1,59 @@
 package teste;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 import java.util.stream.Collectors;
+
+import teste.entities.Employee;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		List<String> names = new ArrayList<String>();
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
-		names.add("Jose");
-		names.add("Orlando");
-		names.add("Ferreira");
+		System.out.print("Enter the file path: ");
+		String path = sc.nextLine();
 
-		// names.sort((n1, n2) -> n1.compareTo(n2));
+		System.out.print("Enter the salary: ");
+		Double salary = sc.nextDouble();
 
-		List<String> names2 = names.stream().map(x -> x.toUpperCase()).collect(Collectors.toList());
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-		names2.forEach(System.out::println);
+			String line = br.readLine();
+
+			List<Employee> list = new ArrayList<>();
+
+			while (line != null) {
+
+				String fields[] = line.split(",");
+				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
+			}
+
+			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary));
+
+			List<String> emails = list.stream().filter(x -> x.getSalary() > salary).map(x -> x.getEmail()).sorted()
+					.collect(Collectors.toList());
+
+			emails.forEach(System.out::println);
+
+			double sum = list.stream().filter(x -> x.getName().charAt(0) == 'M').map(x -> x.getSalary()).reduce(0.0,
+					(x, y) -> x + y);
+
+			System.out.println("Sum of salary of whose people name starts with 'M': " + String.format("%.2f", sum));
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		sc.close();
 
 	}
 
